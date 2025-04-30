0c0e871831d87c282b0d02091916b8e220b8e812
mohit-yad4v <mohitkumaryadav111@gmail.com>
2025-04-30 21:10:29 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import problemRoutes from "./routes/problem.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello Guys welcome to leetlab🔥");
+});
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port 8080");
+});
